428a7bcb046ca3a842e44555a3e8e4b27400911c
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2022-06-28 11:19:32 -0700

    completed

---
+++ main.js
@@ -0,0 +1,24 @@
+let para = document.querySelector('p');
+para.style.color = 'lightblue';
+para.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+let heading1 = document.querySelector('h1');
+heading1.style.fontSize = '48px';
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = '0.25';
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = "I say, 'Hi!'";
+
+let image = document.querySelector('img');
+image.src = 'https://i.imgflip.com/32p9tl.jpg';
+image.style.height = '300px';
+
+let catImage = document.querySelector('#catImage');
+catImage.style.height = '300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = 'item';
+item16.innerText = 'Wont get fooled again.';
+
